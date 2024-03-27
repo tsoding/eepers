51800e6f576a20544c5c787dedb2e3faa68c5b1b
rexim <reximkut@gmail.com>
2024-03-28 03:12:44 +0700

    Pass -pthread to linker

--- build-linux.sh
+++ build-linux.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-gnatmake -f -O3 -Wall -Wextra -gnat2012 eepers.adb -pthread -bargs -static -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm
+gnatmake -f -O3 -Wall -Wextra -gnat2012 eepers.adb -bargs -static -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm  -pthread
 ./eepers
 
 # gnatmake -f -Wall -Wextra -gnat2022 test.adb -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm
