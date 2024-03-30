d90192d08e4cfaf98dd0db43813a95be86adb362
rexim <reximkut@gmail.com>
2024-03-30 08:08:18 +0700

    Rename eepers executable on Linux to eepers-linux

--- build-linux.sh
+++ build-linux.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-gnatmake -f -O3 -Wall -Wextra -gnat2012 eepers.adb -bargs -static -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm  -pthread
-./eepers
+gnatmake -f -O3 -Wall -Wextra -gnat2012 -o eepers-linux eepers.adb -bargs -static -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm  -pthread
+./eepers-linux
 
 # gnatmake -f -Wall -Wextra -gnat2022 test.adb -largs -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -lm
 # ./test
